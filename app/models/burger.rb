bf6d00b2295744ff485474311310212fa27e3005
lucas.ripoche <lucas.ripoche@jaguar-network.com>
2020-03-07 20:33:18 +0100

    Fix nutriment function in burger model

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -2,8 +2,11 @@ class Burger < ApplicationRecord
 
     #Call Openfoodfacts API for product's nutriment
     def nutriments
-        code = self.code
-        product = Openfoodfacts::Product.get(code, locale: 'fr')
-        return product.nutriments.to_hash
+        product = Openfoodfacts::Product.get(self.code, locale: 'fr')
+        if product
+            return product.nutriments.to_hash
+        else
+            return ["nutriment" => "Could not retrieve nutriments, product code must be erroned"]
+        end
     end
 end
--- app/views/burgers/show.json.jbuilder
+++ app/views/burgers/show.json.jbuilder
@@ -1,5 +1,6 @@
 json.partial! "burgers/burger", burger: @burger
 
+#Add product nutriments to product's JSON page 
 json.nutriments do
     json.array! @burger.nutriments  
   end
